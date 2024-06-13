1ef04db996d075246e05543e1f948e3a900dc224
sabashukvaniii <sshukvanii@gmail.com>
2024-06-13 23:34:21 +0400

    Add files via upload

--- classwork.py
+++ classwork.py
@@ -1,11 +1,18 @@
-print (5 > 4)
+#here we used snake_case to make the variable more readable
+dog_name1 = "rick"
+dog_name2 = "john"
+dog_name2 = "max"
+#here we gave a variable a name 
 
-print (5 < 10)
+#but here we used f string that made our day easier because instead of using " " to get some space inbetween to words we just used the spacebar
 
-print (5 == 6)
+print(f"first dogs name is {dog_name1} and second dogs name is {dog_name2}")
 
-print( 5 == 5)
+#so in total we used (snake_case and comments)
 
-print(5 > 4 or 5 < 4)
 
-print (5 > 10 or 5 > 10)
+num1 = input("your name: ")
+
+num2 = input("your surname: ")
+
+print( num1 + num2)
